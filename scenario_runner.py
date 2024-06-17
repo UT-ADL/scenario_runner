e691f078d395b71f697ac6d8415733c750c61cfb
Mahir Gulzar <mahirgulzar@gmail.com>
2024-06-18 01:52:53 +0500

    Reverted output parameter back to its original form i.e. not set by default

--- scenario_runner.py
+++ scenario_runner.py
@@ -551,7 +551,7 @@ def main():
         '--agent', help="Agent used to execute the scenario. Currently only compatible with route-based scenarios.")
     parser.add_argument('--agentConfig', type=str, help="Path to Agent's configuration file", default="")
 
-    parser.add_argument('--output', default=True, help='Provide results on stdout')
+    parser.add_argument('--output', action="store_true", help='Provide results on stdout')
     parser.add_argument('--file', action="store_true", help='Write results into a txt file')
     parser.add_argument('--junit', action="store_true", help='Write results into a junit file')
     parser.add_argument('--json', action="store_true", help='Write results into a JSON file')
